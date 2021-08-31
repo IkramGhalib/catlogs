fda0109351d10bc74421826d6e52340b81ccaf30
IkramGhalib <ikramghalib@gmail.com>
2021-08-31 12:39:55 +0500

    Product model creation

---
+++ lib/models/catlogs.dart
@@ -0,0 +1,23 @@
+class Items{
+  final String name;
+  final String id;
+  final String desc;
+  final num price;
+  final String color;
+  final String imageUrl;
+
+  Items({required this.name, required this.id, required this.desc, required this.price, required this.color, required this.imageUrl});
+
+
+
+}
+
+final Product=[Items(
+    id: "Check code2",
+  name:"IPhone Pro max",
+  desc: "Mobile Phone of Apple Company",
+    imageUrl: 'Image.png',
+    color: "#356eee",
+    price: 22
+
+)];
