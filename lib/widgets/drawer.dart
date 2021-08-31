f50535e28372a9f95b3015bb71f5bdca8b18b451
IkramGhalib <ikramghalib@gmail.com>
2021-08-31 10:55:35 +0500

    Drawer profile modi

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
@@ -5,17 +6,50 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final imageUrl="https://www.google.com/search?q=ikramghalib&rlz=1C1SQJL_enPK961PK961&sxsrf=AOaemvJeSe1nBs_yRPM-OPEwiiXKWWVb2A:1630387811400&source=lnms&tbm=isch&sa=X&ved=2ahUKEwidsuDyw9ryAhWlmFwKHSoEBBcQ_AUoAXoECAEQAw&biw=1366&bih=625#imgrc=a7KtTjyeR1m8OM";
 
     return Drawer(
-      child: ListView(
-        children: [
-          DrawerHeader(
-              child: UserAccountsDrawerHeader(
-                accountName: Text("Ikram"),
-                accountEmail: Text("IkramGhalib@gmail.com"),
-              )
-          )
-        ],
+      child: Container(
+        color: Colors.red,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+
+                child: UserAccountsDrawerHeader(
+                  decoration: BoxDecoration(color: Colors.red),
+                  accountName: Text("Ikram"),
+                  accountEmail: Text("IkramGhalib@gmail.com"),
+                  // currentAccountPicture: Image.network(imageUrl),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: AssetImage("assets/images/login.png"),
+                    // backgroundImage: NetworkImage(imageUrl),
+
+                  ),
+                )
+            ),
+            //Left icon and title
+            ListTile(
+              leading:
+              Icon(
+                CupertinoIcons.profile_circled,color: Colors.white,),
+              title: Text("Profile",textScaleFactor: 1.5,style: TextStyle(color: Colors.white),),
+            ),
+            ListTile(
+              leading:
+              Icon(
+                CupertinoIcons.settings,color: Colors.white,),
+              title: Text("Sitting",textScaleFactor: 1.5,style: TextStyle(color: Colors.white),),
+            ),
+            ListTile(
+              leading:
+              Icon(
+                CupertinoIcons.bell,color: Colors.white,),
+              title: Text("Notification",textScaleFactor: 1.5,style: TextStyle(color: Colors.white),),
+            )
+          ],
+        ),
       ),
     );
   }
